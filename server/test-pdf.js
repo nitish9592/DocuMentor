e1251d141f42b9cb19ac999fe451a118cc6cfdb2
Nitish <Nitishb9592@gmail.com>
2025-07-08 23:29:01 +0530

    delete test pdf

--- server/test-pdf.js
+++
@@ -1,8 +0,0 @@
-// test-pdf.js
-import fs from 'fs';
-import pdfParse from 'pdf-parse';
-
-const buffer = fs.readFileSync('./uploads/yourfile.pdf');
-pdfParse(buffer).then(data => {
-  console.log(data.text);
-});
